0a920a774622864211027c03bf0f2262420c9a9f
thinhnv789 <thinhnv.vnu@gmail.com>
2018-01-10 21:24:33 +0700

    authenticate

--- src/apis/routes/slider.js
+++ src/apis/routes/slider.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 var sliderController = require('./../controllers/Slider');
 
+/**
+ * API keys and Passport configuration.
+ */
+const passport = require('./../../middleware/passport');
+
+
 /* GET users listing. */
-router.get('/home-slider', sliderController.getHomeSlider);
+router.get('/home-slider', passport.isAuthenticated, sliderController.getHomeSlider);
 
 
 module.exports = router;

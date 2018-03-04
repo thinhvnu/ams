0944c8b7c74613c7c95cb0aab6f9de788c307fce
thinhnv789 <thinhnv.vnu@gmail.com>
2018-03-04 18:06:02 +0700

    t

--- src/controllers/ApartmentBuildingGroup.js
+++ src/controllers/ApartmentBuildingGroup.js
@@ -98,6 +98,89 @@ exports.postCreate = (req, res, next) => {
 	});
 }
 
+exports.getEdit = (req, res, next) => {
+	ApartmentBuildingGroup.findById(req.params.abgId)
+		.populate({
+			path: 'manager',
+			model: 'User',
+			select: {'_id': 1, 'userName': 1}
+		})
+		.populate({
+			path: 'createdBy',
+			model: 'User',
+			select: {'userName': 1}
+		})
+		.populate({
+			path: 'apartmentBuildings',
+			model: 'ApartmentBuilding',
+			populate: {
+				path: 'manager',
+				model: 'User',
+				select: { 'userName': 1 }
+			}
+		})
+		.exec(function (err, abg) {
+			if (err) {
+				console.log('err', err)
+				return next(err);
+			}
+			console.log('manager', abg.manager._id);
+			User.find({}, (err, users) => {
+				res.render('apartment-building-group/edit', {
+					title: abg.abgName,
+					current: ['apartment-building-group', 'edit'],
+					users: users,
+					data: abg
+				});
+			})
+		});
+}
+
+exports.postUpdate = (req, res, next) => {
+	req.checkBody('abgName', 'Tên khu chung cư không được để trống').notEmpty();
+	req.checkBody('manager', 'Chọn quản lý').notEmpty();
+	req.checkBody('address', 'Địa chỉ không được để trống').notEmpty();
+	
+	req.getValidationResult().then(function (errors) {
+		if (!errors.isEmpty()) {
+			var errors = errors.mapped(), data = Object.assign(req.body, {id: req.params.abgId});
+
+			console.log('err', errors);
+			User.find({}, (err, users) => {
+				return res.render('apartment-building-group/edit', {
+					title: 'Cập nhật dữ liệu khu chung cư',
+					current: ['apartment-building-group', 'edit'],
+					users: users,
+					errors: errors,
+					data: req.body
+				});
+			})
+		} else {
+			ApartmentBuildingGroup.findById(req.params.abgId, (err, abg) => {
+				if (err) {
+					req.flash('errors', 'Cập nhật dữ liệu không thành công');
+					return res.redirect('/apartment-building-group/edit/' + req.params.abgId);
+				}
+
+				abg.abgName = req.body.abgName;
+				abg.address = req.body.address;
+				abg.manager = req.body.manager;
+				abg.status = req.body.status;
+				abg.updatedBy = req.session.user._id;
+
+				abg.save((err) => {
+					if (err) {
+						req.flash('errors', 'Cập nhật dữ liệu không thành công');
+						return res.redirect('/apartment-building-group/edit/' + req.params.abgId);
+					}
+					req.flash('success', 'Cập nhật dữ liệu thành công');
+					return res.redirect('/apartment-building-group');
+				});
+			})
+		}
+	});
+}
+
 /**
  * 
  * @param {*} req : abgId
@@ -160,4 +243,15 @@ exports.getView = (req, res, next) => {
 				});
 		}
 	});
+}
+
+exports.getDelete = (req, res, next) => {
+	ApartmentBuildingGroup.remove({_id: req.params.abgId}, (err) => {
+		if (err) {
+			req.flash('errors', 'Có lỗi xảy ra');
+		} else {
+			req.flash('success', 'Xóa thành công');
+		}
+		return res.redirect('/apartment-building-group');
+	})
 }
--- src/routes/apartmentBuildingGroup.js
+++ src/routes/apartmentBuildingGroup.js
@@ -17,8 +17,13 @@ router.get('/create', passport.isAuthenticated, abgController.getCreate);
 /* API create new abg */
 router.post('/create', passport.isAuthenticated, abgController.postCreate);
 
+/* Get edit  */
+router.get('/edit/:abgId', passport.isAuthenticated, abgController.getEdit);
+
+/** Post update */
+router.post('/update/:abgId', passport.isAuthenticated, abgController.postUpdate);
+
 /* Get view */
 router.get('/view/:abgId', passport.isAuthenticated, abgController.getView);
-
 
 module.exports = router;

a84c364686ad85467345634f1f0a1c71b5ae035e
thinhnv789 <thinhnv.vnu@gmail.com>
2018-05-12 17:39:42 +0700

    t

--- src/apis/controllers/Chat.js
+++ src/apis/controllers/Chat.js
@@ -383,8 +383,8 @@ exports.getClients = (req, res, next) => {
                                                     isRead: false
                                                 }).exec((err, gMessUnread) => {
                                                     // gs[i].messUnread = gMessUnread;
-                                                    gs[i] = gs[i].toObject();
-                                                    gs[i] = Object.assign(gs[i].toObject(), {messUnread: gMessUnread});
+                                                    // gs[i] = gs[i].toObject();
+                                                    gs[i] = Object.assign(gs[i], {messUnread: gMessUnread});
                                                     count++;
 
                                                     if (count >= (users.length + gs.length)) {

a20d623e856753ea212ff66fb85871779b334f34
Thinhnv789 <thinhnv.vnu@gmail.com>
2018-05-31 20:38:52 +0700

    share page

--- src/apis/controllers/Dashboard.js
+++ src/apis/controllers/Dashboard.js
@@ -3,6 +3,6 @@ exports.getSharePage = function (req, res) {
 	res.json({
         success: true,
         errorCode: '0',
-        data: 'You need - We have \n https://play.google.com/store/apps/details?id=com.tapdoanviet'
+        data: 'HOMESUN - MỌI GIA ĐÌNH KHÔNG THỂ THIẾU \n Nhận ngay 200k cho lần cài Apps đầu tiên \n https://play.google.com/store/apps/details?id=com.tapdoanviet \n  https://itunes.apple.com/us/app/homesun/id1372132641?l=vi&ls=1&mt=8'
     })
 };

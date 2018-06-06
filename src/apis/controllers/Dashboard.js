3a731d97ce1a8a35b17f240a678ba4a498fef767
Thinhnv789 <thinhnv.vnu@gmail.com>
2018-06-06 23:01:29 +0700

    t

--- src/apis/controllers/Dashboard.js
+++ src/apis/controllers/Dashboard.js
@@ -3,6 +3,6 @@ exports.getSharePage = function (req, res) {
 	res.json({
         success: true,
         errorCode: '0',
-        data: 'HOMESUN - MỌI GIA ĐÌNH KHÔNG THỂ THIẾU \n Nhận ngay 200k cho lần cài Apps đầu tiên \n https://play.google.com/store/apps/details?id=com.tapdoanviet \n  https://itunes.apple.com/us/app/homesun/id1372132641?l=vi&ls=1&mt=8'
+        data: 'HOMESUN - MỌI GIA ĐÌNH KHÔNG THỂ THIẾU \n Nhận ngay 200k cho lần cài Apps đầu tiên \n Link cài trên CH Play: https://play.google.com/store/apps/details?id=com.tapdoanviet \n  Link cài trên App store: https://itunes.apple.com/us/app/homesun/id1372132641?l=vi&ls=1&mt=8'
     })
 };
